f47354e23cfac886f3cf2ab4be641a7cdf8b5b1a
Marc Garcia <mgarcia@atlasit.com>
2011-05-09 17:11:07 +0200

    changes for distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
 
-setup(
-    name='viri',
+setup(name='viri',
     version='0.0.1',
-    description='Distributed system for remote execution of tasks',
+    description='System for remotely executing Python scripts',
     author='Marc Garcia',
     author_email='garcia.marc@gmail.com',
-    py_modules=['virid', 'viric'],
+    url='http://www.viriproject.com',
+    data_files=(
+        ('/etc/viri', ('etc/viri/virid.conf',)),
+        ('/etc/init.d', ('etc/init.d/virid',)),
+    ),
+    scripts=('bin/virid', 'bin/viric'),
+    packages=('viri',),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: System :: Systems Administration',
+    )
 )
 
