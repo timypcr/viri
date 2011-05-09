2a0328874003e2b5f2b4043173c37885bd33196c
Marc Garcia <garcia.marc@gmail.com>
2011-05-09 20:58:31 +0200

    project description changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='viri',
     version='0.0.1',
-    description='System for remotely executing Python scripts',
+    description='Remote execution of Python scripts',
     author='Marc Garcia',
     author_email='garcia.marc@gmail.com',
     url='http://www.viriproject.com',
