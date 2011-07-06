744d2468aef972be6df717341c63dabbb0eac244
Marc Garcia <marc.garcia@ntt.eu>
2011-07-06 12:25:10 +0200

    some more structural changes

--- virid/bin/hello.py
+++
@@ -1,8 +0,0 @@
-"""Minimal Viri script for testing and to be used as a script sample. It
-always return 'echo'.
-"""
-
-class ViriScript:
-    def run(self, name):
-        return 'Hello {}!'.format(name)
-
