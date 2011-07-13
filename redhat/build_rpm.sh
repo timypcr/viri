f5b9a42f317d5c58f6bce8486a6ba39352e4640d
Marc Garcia <marc.garcia@ntt.eu>
2011-07-13 11:29:08 +0200

    script for creating tarball improved to create the rpm

---
+++ redhat/build_rpm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ ! $1 ]
+then
+    echo "Version argument missing"
+    exit 1
+fi
+
+if [ ! `which rpmbuild` ]
+then
+    echo "rpmbuild not found. Install it before running this script."
+    exit 1
+fi
+
+VERSION=$1
+PROJECT_NAME=Viri-$1
+
+if [[ `cat /proc/version` == *Red\ Hat* ]]
+then
+    RPM_ROOT=/usr/src/redhat
+elif [[ `cat /proc/version` == *Debian* ]]
+then
+    RPM_ROOT=~/rpmbuild
+else
+    echo "This script only works in Red Hat or Debian. Aborting."
+    exit 1
+fi
+
+mkdir -p $RPM_ROOT/SOURCES
+mkdir -p $RPM_ROOT/RPMS/noarch
+
+cp viri.spec $RPM_ROOT/SPECS
+cp -r ../virid $RPM_ROOT/SOURCES/$PROJECT_NAME
+cd $RPM_ROOT/SOURCES/
+tar -cjvf $RPM_ROOT/SOURCES/$PROJECT_NAME.tar.bz2 $PROJECT_NAME
+rm -rf $RPM_ROOT/SOURCES/$PROJECT_NAME
+rpmbuild -bb $RPM_ROOT/SPECS/viri.spec
+
