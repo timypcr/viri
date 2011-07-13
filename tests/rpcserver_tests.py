e6617f054b5de51f2795bdf2cc7bd3dbe3aae3da
Marc Garcia <marc.garcia@ntt.eu>
2011-07-13 15:21:28 +0200

    See #21. Working on tests, mostly to the rpcserver module.

--- tests/rpcserver_tests.py
+++ tests/rpcserver_tests.py
@@ -15,16 +15,46 @@
 # along with Viri.  If not, see <http://www.gnu.org/licenses/>.
 
 r"""
+>>> import os
 >>> import tempfile
 >>> import shutil
 >>> import xmlrpc.client
 >>> from viri import orm, objects, rpcserver
 
->>> db_file = tempfile.NamedTemporaryFile()
+>>> SCRIPT_SYNTAX_ERROR = b'''
+... print('syntax error
+... '''
+
+>>> SCRIPT_NO_CLASS = b'''
+... base_attr = True
+... '''
+
+>>> SCRIPT_NO_RUN = b'''
+... class ViriScript:
+...     base_attr = True
+... '''
+
+>>> SCRIPT_CORRECT = b'''
+... class ViriScript:
+...     def run(self):
+...         return 'Hello world!'
+... '''
+
+>>> def parse_ls(file_list):
+...     result = []
+...     for line in file_list.split('\n'):
+...         line_arr = line.split('\t')
+...         line_arr.pop() # remove saved date
+...         result.append('\t'.join(line_arr))
+...     result.sort()
+...     return '\n'.join(result)
+
+>>> db_file = tempfile.NamedTemporaryFile(prefix='viri_db_')
 >>> db = orm.Database(db_file.name)
 >>> context = dict(conf={}, db=db)
 >>> for obj in objects.objects:
->>> context[obj.__name__] = obj
+...     obj.create_table(db)
+...     context[obj.__name__] = obj
 
 >>> rpcs = rpcserver.RPCServer(
 ...     port=6808,
@@ -33,113 +63,56 @@ r"""
 ...     db=db,
 ...     context=context)
 
-#################################
-### put and get of data files ###
-#################################
 
->>> rpcs.put({'file_name': 'filename1',
-...     'file_content': xmlrpc.client.Binary(b'filaname1 content\n'),
-...     'data': True})
-(0, 'Data file filename1 saved')
->>> rpcs.put({'file_name': 'filename2.txt',
-...     'file_content': xmlrpc.client.Binary(b'filaname2 content\n'),
-...     'data': True})
-(0, 'Data file filename2.txt saved')
->>> rpcs.put({'file_name': 'filename3.py',
-...     'file_content': xmlrpc.client.Binary(b'print("hello world!")\n'),
-...     'data': True})
-(0, 'Data file filename3.py saved')
->>> rpcs.get({'filename_or_id': 'filename1', 'data': True})[1].data
-b'filaname1 content\n'
->>> rpcs.put({'file_name': 'filename1',
-...     'file_content': xmlrpc.client.Binary(b'filaname1 content modified\n'),
-...     'data': True})
-(0, 'Not overwriting file filename1')
->>> rpcs.get({'filename_or_id': 'filename1', 'data': True})[1].data
-b'filaname1 content\n'
->>> rpcs.put({'file_name': 'filename1',
-...     'file_content': xmlrpc.client.Binary(b'filaname1 content modified\n'),
-...     'data': True, 'overwrite': True})
-(0, 'Data file filename1 overwrote')
->>> rpcs.get({'filename_or_id': 'filename1', 'data': True})[1].data
-b'filaname1 content modified\n'
->>> ls_res = rpcs.ls({'data': True})[1].split('\n')
->>> ls_res.sort()
->>> ls_res
-['filename1', 'filename2.txt', 'filename3.py']
+# Put data files
+>>> rpcs.put({'file_name': 'data_file_1',
+...     'file_content': xmlrpc.client.Binary(b'file 1 content\n')})
+(0, '0fdeee3bf255a30a207a3c84934defb9eb00efb0')
+>>> rpcs.put({'file_name': 'data_file_2.txt',
+...     'file_content': xmlrpc.client.Binary(b'file 2 content\n')})
+(0, '19c32940f2e5f997bc34530cf9b544247e674168')
 
-##################################
-### operations with data files ###
-##################################
+# List and get data files
+>>> parse_ls(rpcs.ls({})[1])
+'data_file_1\t0fdeee3bf255a30a207a3c84934defb9eb00efb0\ndata_file_2.txt\t19c32940f2e5f997bc34530cf9b544247e674168'
+>>> rpcs.get({'file_name_or_id': 'data_file_1'})[1].data
+b'file 1 content\n'
+>>> rpcs.get({'file_name_or_id': '0fdeee3bf255a30a207a3c84934defb9eb00efb0'})[1].data
+b'file 1 content\n'
+>>> rpcs.get({'file_name_or_id': 'data_file_2.txt'})[1].data
+b'file 2 content\n'
+>>> rpcs.get({'file_name_or_id': '19c32940f2e5f997bc34530cf9b544247e674168'})[1].data
+b'file 2 content\n'
+>>> rpcs.get({'file_name_or_id': 'not_sent_data_file'})
+(1, 'File not found')
 
->>> rpcs.mv({'source': 'filename1', 'destination': 'filename4'})
-(0, 'File filename1 successfully renamed to filename4')
->>> rpcs.mv({'source': 'filename1', 'destination': 'filename4'})
-(1, 'File filename1 not found')
->>> rpcs.mv({'source': 'filename4', 'destination': 'filename2.txt'})
-(1, 'Rename aborted because destination file already exists. Use --overwrite to force it')
->>> rpcs.mv({'source': 'filename4', 'destination': 'filename2.txt', 'overwrite': True})
-(0, 'Existing file filename2.txt replaced by renaming filename4')
->>> rpcs.rm({'filename': 'filename2.txt'})
-(0, 'File filename2.txt successfully removed')
->>> rpcs.rm({'filename': 'filename2.txt'})
-(1, 'File not found. File names cannot include directories')
->>> ls_res = rpcs.ls({'data': True})[1].split('\n')
->>> ls_res.sort()
->>> ls_res
-['filename3.py']
+# Put scripts
+>>> rpcs.put({'file_name': 'syntax_error.py',
+...     'file_content': xmlrpc.client.Binary(SCRIPT_SYNTAX_ERROR)})
+(0, 'f8b8b3454691870fa1bbd323698802ee29155cb6')
+>>> rpcs.put({'file_name': 'no_class.py',
+...     'file_content': xmlrpc.client.Binary(SCRIPT_NO_CLASS)})
+(0, '8d216cb3d41bca080407eebb96384f941d4da793')
+>>> rpcs.put({'file_name': 'no_run.py',
+...     'file_content': xmlrpc.client.Binary(SCRIPT_NO_RUN)})
+(0, '4a1f32f97b8691985630251c9f53654483aeca61')
+>>> rpcs.put({'file_name': 'no_run.py',
+...     'file_content': xmlrpc.client.Binary(SCRIPT_CORRECT)})
+(0, '6bd4e019d841ea7716ec0cd3be86b878edca47ab')
 
-##############################
-### put and get of scripts ###
-##############################
+# List and get scripts
+>>> parse_ls(rpcs.ls({})[1])
+'data_file_1\t0fdeee3bf255a30a207a3c84934defb9eb00efb0\ndata_file_2.txt\t19c32940f2e5f997bc34530cf9b544247e674168\nno_class.py\t8d216cb3d41bca080407eebb96384f941d4da793\nno_run.py\t4a1f32f97b8691985630251c9f53654483aeca61\nno_run.py\t6bd4e019d841ea7716ec0cd3be86b878edca47ab\nsyntax_error.py\tf8b8b3454691870fa1bbd323698802ee29155cb6'
 
->>> rpcs.get({'filename_or_id': 'script1.py',})
-(1, 'File not found. File names cannot include directories')
->>> rpcs.put({'file_name': 'script1.py',
-...     'file_content': xmlrpc.client.Binary(b'print("hello world!")\n')})
-(0, '5db8eb03071c8c231a8f51b3d2a98bd1eb589634')
->>> rpcs.get({'filename_or_id': 'script1.py',})[1].data
-b'print("hello world!")\n'
->>> rpcs.put({'file_name': 'script1.py',
-...     'file_content': xmlrpc.client.Binary(b'print("hello viri!")\n')})
-(0, 'e93c885fc9865db7369d9b516e839a789e8a1622')
-
-##########################
-### script definitions ###
-##########################
-
->>> BASE_CORRECT = '''
-class ViriScript:
-    base_attr = True
-'''
->>> SCRIPT_CORRECT = '''
-class ViriScript:
-    def run(self):
-        return '__base__ used: %s ' % hasattr(self, 'base_attr')
-'''
->>> SCRIPT_NO_CLASS = '''
-base_attr = True
-'''
->>> SCRIPT_SYNTAX_ERROR = '''
-print('syntax error
-'''
-
-####################################
-### script execution and history ###
-####################################
-
+# Script execution
 >>> rpcs.history({})
 (0, '')
->>> rpcs.execute({'script_id': 'e93c885fc9865db7369d9b516e839a789e8a1622', 'use_id': True})
-(1, '')
-
-################
-### clean up ###
-################
-
->>> shutil.rmtree(script_dir)
->>> shutil.rmtree(data_dir)
->>> shutil.rmtree(info_dir)
+>>> rpcs.execute({'file_name_or_id': 'not_sent_script', 'args': ''})
+(1, 'File not found')
+>>> rpcs.execute({'file_name_or_id': 'not_sent_script.py', 'args': ''})
+(1, 'File not found')
+>>> rpcs.execute({'file_name_or_id': 'syntax_error.py', 'args': ''})
+(1, 'Traceback (most recent call last):\n  File "syntax_error.py", line 2\n    print(\'syntax error\n                      ^\nSyntaxError: EOL while scanning string literal\n')
 
 """
 
