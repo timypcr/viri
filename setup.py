e4181e9729419f943445785e07c228d066597ce8
Jesús Corrius <jcorrius@gmail.com>
2011-08-17 11:54:30 +0200

    Linux is not the only OS in the world

--- setup.py
+++ setup.py
@@ -16,7 +16,16 @@
 # You should have received a copy of the GNU General Public License
 # along with Viri.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from setuptools import setup
+
+is_windows = sys.platform == 'win32'
+
+datafiles = (
+        ('/usr/sbin', ('bin/virid', 'bin/virid-conf')),
+        ('/usr/bin', ('bin/viric',)),
+        ('/etc/viri', ('conf/virid.conf',)),
+    ) if not is_windows else ()
 
 kwargs = dict(name='viri',
     version='0.1',
@@ -25,11 +34,7 @@ kwargs = dict(name='viri',
     author_email='garcia.marc@gmail.com',
     url='http://www.viriproject.com',
     packages=('libviri',),
-    data_files=(
-        ('/usr/sbin', ('bin/virid', 'bin/virid-conf')),
-        ('/usr/bin', ('bin/viric',)),
-        ('/etc/viri', ('conf/virid.conf',)),
-    ),
+    data_files=datafiles,
     classifiers=(
         'Development Status :: 4 - Beta',
         'Environment :: No Input/Output (Daemon)',
