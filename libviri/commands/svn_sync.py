6de92f0de152d6223bd9e0465c8fb62ca5aa550e
Jesús Corrius <jcorrius@gmail.com>
2011-10-17 11:25:23 +0200

    Allow username and password in svn_sync

--- libviri/commands/svn_sync.py
+++ libviri/commands/svn_sync.py
@@ -21,19 +21,32 @@ class Local:
         parser.add_argument('svn_server_path', help='svn server path of the file to sync')
         parser.add_argument('remote_path', help='destination path where to '
             'sync the file in remote hosts')
+        parser.add_argument('-u', '--username', dest='username', action='store', 
+            help='username to access svn repository')
+        parser.add_argument('-p', '--password', dest='password', action='store',
+            help='password to access svn repository')
         parser.add_argument('-sv', '--sversion', type=int, dest='sversion',
             help='Subversion version of the file in the repository')
             
-    def run(self, svn_server_path, remote_path, sversion):                    
+    def run(self, svn_server_path, remote_path, username, password, sversion):                    
         import os, tempfile
      
         file_name = os.path.basename(svn_server_path)
         tmp_file_name = tempfile.gettempdir() + '/' + file_name
         
-        if sversion:
-            os.system('svn export -r {0} {1} {2}'.format(sversion, svn_server_path, tmp_file_name))
+        if username and password:
+            if sversion:
+                os.system('svn export --username {0} --password {1} -r {2} {3} {4}'.format(username, password, sversion, svn_server_path, tmp_file_name))
+            else:
+                os.system('svn export --username {0} --password {1} {2} {3}'.format(username, password, svn_server_path, tmp_file_name))
         else:
-            os.system('svn export {0} {1}'.format(svn_server_path, tmp_file_name))
+            if sversion:
+                os.system('svn export -r {0} {1} {2}'.format(sversion, svn_server_path, tmp_file_name))
+            else:
+                os.system('svn export {0} {1}'.format(svn_server_path, tmp_file_name))
+                
+        if not os.path.isfile(tmp_file_name):
+            return (1, None, 'Could not get versioned file from SVN repository.\n')
 
         script_id = self.send_file(__file__)
         file_id = self.send_file(tmp_file_name)
@@ -58,14 +71,11 @@ class Remote:
     def save_file(self, file_id, path):
         import os
         
-        if os.path.isfile(path):
-            self.File.save_content(self.db, file_id, path)
-            sha1_filename = self.get_sha1sum_filename(path)
-            with open(sha1_filename, mode='w', encoding='utf-8') as f:
-                f.write(file_id)    
-            return 'Remote file saved.\n'
-        else:
-            return 'Remote file does not exist.\n'
+        self.File.save_content(self.db, file_id, path)
+        sha1_filename = self.get_sha1sum_filename(path)
+        with open(sha1_filename, mode='w', encoding='utf-8') as f:
+            f.write(file_id)    
+        return 'Remote file saved.\n'
 
     def run(self, file_id, path):
         import os
